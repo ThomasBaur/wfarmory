021722b9d19a1020efc4e57644ec9c5478094d2c
Thomas Baur <thomas.baur@worldline.com>
2018-10-09 15:25:36 +0200

    Removed file headers

--- src/main/java/wfarmory/Equippable.java
+++ src/main/java/wfarmory/Equippable.java
@@ -1,14 +1,4 @@
 package wfarmory;
-/*
- ******************************************************************************
- *      Worldline Germany GmbH             Security Solutions
- *      Pascalstrasse 19                   Phone +49 (0) 2408/148-0
- *      D-52076 Aachen
- *
- *      All rights reserved
- *
- ******************************************************************************
- */
 
 public interface Equippable {
 }
